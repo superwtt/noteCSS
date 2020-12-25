f4def993b6a30012fd6277ed318fa11cec99a0d8
superwtt <keepgowtt@126.com>
2020-12-25 11:53:45 +0800

    新增svg 的加载动画

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -359,8 +359,8 @@ module.exports = {
         collapsable: false,
         children: [
           {
-            title: "SVG实现按钮加载动画",
-            path: "/svg/SVG实现按钮加载动画.html",
+            title: "SVG实现按钮动画",
+            path: "/svg/SVG实现按钮动画.html",
           }
         ],
       },
